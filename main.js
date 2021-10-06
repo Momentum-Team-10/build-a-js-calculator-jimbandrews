88f0f6bb3ec5cfec87ad731a86ed807e87ce14f6
James Andrews <jbandrew14@gmail.com>
2021-10-06 18:41:37 -0400

    fixed html tags, calculator .js code functions

---
+++ main.js
@@ -0,0 +1,32 @@
+let display = document.getElementById('display');
+
+var equalsTest = 0;
+
+// create event for all number and operator buttons
+let buttons = document.querySelectorAll('.button');
+
+for (let button of buttons) {
+    button.addEventListener('click', () => {
+        if (equalsTest === 0) {
+            display.innerText += button.innerText;
+        } else {
+            display.innerText = button.innerText;
+            equalsTest = 0;
+        }
+    })
+}
+
+// create event for equals button
+let equals = document.getElementById('equals')
+
+equals.addEventListener('click', () => {
+    display.innerText = eval(display.innerText);
+    equalsTest = 1;
+})
+
+// create event for clear button
+let clear = document.getElementById('clear')
+
+clear.addEventListener('click', () => {
+    display.innerText = ''
+})
